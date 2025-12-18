80a03e7842d220d76faf8c9202c39271af43685b
koenig12 <koenig12@uni-potsdam.de>
2025-12-18 11:26:41 +0100

    Update code

--- ISS_PDBT_both_bounds.m
+++ ISS_PDBT_both_bounds.m
@@ -20,7 +20,7 @@ sig_obs_long    = repmat(sig_obs,n,1);
 
 %% given low-rank prior - sample covariance of compatible covariance
 ensemble_size   = 90; % size of prior ensemble
-Lyap_solution   = lyapchol(A,B)'; % compatibility Lyapunov eq.
+Lyap_solution   = lyapchol(A,B)'; % compatibility Lyapunov equation
 ensemble        = Lyap_solution * randn(d,ensemble_size); 
 % ensemble drawn from compatible covariance 
 Gamma_pr        = cov(ensemble'); % prior is ensemble covariance
@@ -37,6 +37,7 @@ figure; clf
 scaling_factors = [0.1,1,10];
 for scale_count = 1:length(scaling_factors)
     L_pr    = scaling_factors(scale_count)*Gamma_pr_root;
+    % prior for experiment is scaled version of Gamma_pr
 
     %% compute infinite noisy observability Gramian
     % helper matrix    
@@ -57,7 +58,7 @@ for scale_count = 1:length(scaling_factors)
     L_P         = real(sqrtm(P));
     
     %% set up the experiment
-    % generate random data from multiple initial conditions
+    % generate random initial condition
     x0          = L_pr*randn(size(L_pr,2),1);
     
     % define full forward model 
@@ -75,7 +76,7 @@ for scale_count = 1:length(scaling_factors)
     % helper matrix
     M   = G*L_pr;
     
-    % generate measurements
+    % generate measurements and add noise
     y   = G*x0;
     m   = y + sig_obs_long.*randn(n*d_out,1);
     m_scaled = m./sig_obs_long;
@@ -86,10 +87,10 @@ for scale_count = 1:length(scaling_factors)
     PosMean_true    = PosCov_true*full_rhs;
     
     %% compute posterior approximations and errors
-    r_vals      = 1:2:prior_rank/2;
+    r_vals      = 1:2:(prior_rank/2);
     rmax        = max(r_vals);
     
-    % infinite prior-driven balancing (PD)
+    % prior-driven balancing (PD-BT)
     [V,S,W]     = svd(L_Q_inf'*L_P_inf); 
     % compute balanced quantities for posterior computation and error bound
     delQ_inf    = diag(S);
@@ -100,29 +101,29 @@ for scale_count = 1:length(scaling_factors)
     C_inf       = C*Tr_inf;
     L_pr_inf    = Sr_inf'*L_pr;
     
-    % time-limited prior-driven balancing (PD)
+    % time-limited prior-driven balancing (PD-TLBT)
     [V,S,W]     = svd(L_Q*L_P); 
     % compute balanced quantities for posterior computation and error bound
     delQ        = diag(S);
     Siginvsqrt  = diag(1./sqrt(delQ));
-    Sr_PD       = (Siginvsqrt*V'*L_Q)';
-    Tr_PD       = L_P*W*Siginvsqrt; % balancing transformation
-    A_PD        = Sr_PD'*A*Tr_PD;
-    C_PD        = C*Tr_PD;
-    L_pr_PD     = Sr_PD'*L_pr;
+    Sr_TL       = (Siginvsqrt*V'*L_Q)';
+    Tr_TL       = L_P*W*Siginvsqrt; % balancing transformation
+    A_TL        = Sr_TL'*A*Tr_TL;
+    C_TL        = C*Tr_TL;
+    L_pr_TL     = Sr_TL'*L_pr;
     
     %% compute posterior approximations
-    F_Dist_abs      = zeros(1,length(r_vals)); % PD-TLBT Frobenius error (full space)
-    F_Dist_inf      = zeros(1,length(r_vals)); % infinite PD-BT Frobenius error (full space)
-    mean_Dist       = zeros(1,length(r_vals)); % PD-TLBT error on first mean (full Space)
-    mean_Dist_inf   = zeros(1,length(r_vals)); % infinite PD-BT error on first mean (full Space)
-    Trace_vals      = zeros(1,length(r_vals)); % trace for PD-TLBT error bound
-    Trace_vals_inf  = zeros(1,length(r_vals)); % trace for infinite PD-BT error bound
+    F_Dist_TL       = zeros(1,length(r_vals)); % PD-TLBT Frobenius error (full space)
+    F_Dist_inf      = zeros(1,length(r_vals)); % PD-BT Frobenius error (full space)
+    mean_Dist_TL    = zeros(1,length(r_vals)); % PD-TLBT error on first mean (full Space)
+    mean_Dist_inf   = zeros(1,length(r_vals)); % PD-BT error on first mean (full Space)
+    Trace_vals_TL   = zeros(1,length(r_vals)); % trace for PD-TLBT error bound
+    Trace_vals_inf  = zeros(1,length(r_vals)); % trace for PD-BT error bound
     
     for rr = 1:length(r_vals)
         r = r_vals(rr);
-        %% infinite PD-BT posterior quantities
-        % PD-BT - generate reduced forward matrix G_LIS_BT (not restricted)
+        %% PD-BT posterior quantities
+        % PD-BT - generate reduced forward matrix G_inf
         G_inf               = zeros(n*d_out,r);
         iter                = expm(A_inf(1:r,1:r)*dt_obs);
         temp                = C_inf(:,1:r);
@@ -140,7 +141,7 @@ for scale_count = 1:length(scaling_factors)
         mean_inf            = PosCov_inf*(G_infS./sig_obs_long)'*m_scaled;
         mean_Dist_inf(rr)   = norm(mean_inf-PosMean_true);
         
-        %% infinite PD-BT - assemble necessary quantities for the outpur error bound
+        %% PD-BT - assemble necessary quantities for the outpur error bound
         L2_inf              = L_pr_inf(r+1:d,:);
         C1_inf              = C_inf(:,1:r)./sig_obs;
         A11_inf             = A_inf(1:r,1:r);
@@ -151,44 +152,44 @@ for scale_count = 1:length(scaling_factors)
         Trace_vals_inf(rr)  = trace((L2_inf*L2_inf'+2*Y2_inf*A12_inf)*delQ2_inf);
     
         %% time-limited PD-BT posterior quantities
-        % PD-BT - generate reduced forward matrix G_LIS_BT (not restricted)
-        G_PD            = zeros(n*d_out,r);
-        iter            = expm(A_PD(1:r,1:r)*dt_obs);
-        temp            = C_PD(:,1:r);
+        % PD-BT - generate reduced forward matrix G_TL
+        G_TL            = zeros(n*d_out,r);
+        iter            = expm(A_TL(1:r,1:r)*dt_obs);
+        temp            = C_TL(:,1:r);
         for i = 1:n
             temp                             = temp*iter;
-            G_PD((i-1)*d_out+1:i*d_out,:)   = temp;
+            G_TL((i-1)*d_out+1:i*d_out,:)   = temp;
         end
-        G_PDS           = G_PD*Sr_PD(:,1:r)';
-        M_PD            = G_PD*L_pr_PD(1:r,:); % reduced helper matrix
+        G_TLS           = G_TL*Sr_TL(:,1:r)';
+        M_TL            = G_TL*L_pr_TL(1:r,:); % reduced helper matrix
         
         % PD-BT - compute posterior covariance
-        PosCov_PD       = L_pr*(eye(size(L_pr,2)) - M_PD'*((M_PD*M_PD'+diag(sig_obs_long.^2))\M_PD))*L_pr';
-        F_Dist_abs(rr)  = norm(PosCov_PD-PosCov_true,'fro');
+        PosCov_TL       = L_pr*(eye(size(L_pr,2)) - M_TL'*((M_TL*M_TL'+diag(sig_obs_long.^2))\M_TL))*L_pr';
+        F_Dist_TL(rr)   = norm(PosCov_TL-PosCov_true,'fro');
         % PD-BT - compute posterior mean
-        mean_PD         = PosCov_PD*(G_PDS./sig_obs_long)'*m_scaled;
-        mean_Dist(rr)   = norm(mean_PD(:,1)-PosMean_true(:,1));
+        mean_TL         = PosCov_TL*(G_TLS./sig_obs_long)'*m_scaled;
+        mean_Dist_TL(rr)= norm(mean_TL(:,1)-PosMean_true(:,1));
         
         %% time-limited PD-BT - assemble necessary quantities for the outpur error bound
-        L1      = L_pr_PD(1:r,:);
-        A11     = A_PD(1:r,1:r);
-        C1      = C_PD(:,1:r)./sig_obs;
+        L1      = L_pr_TL(1:r,:);
+        A11     = A_TL(1:r,1:r);
+        C1      = C_TL(:,1:r)./sig_obs;
         % compute mixed Gramian
         right_side_M    = -L_pr*L1'+expm(A*T)*(L_pr*L1')*expm(A11'*T);
         PTM             = real(sylvester(A,A11',right_side_M)); 
         % compute reduced Gramian
-        right_side_r    = -L1*L1'+expm(A11*T)*(L1*L1')*expm(A11'*T);
-        PTr             = real(sylvester(A11,A11',right_side_r)); 
-        Trace_vals(rr)  = trace(F*P*F')+trace(C1*PTr*C1')-2*trace(F*PTM*C1');
+        right_side_r        = -L1*L1'+expm(A11*T)*(L1*L1')*expm(A11'*T);
+        PTr                 = real(sylvester(A11,A11',right_side_r)); 
+        Trace_vals_TL(rr)   = trace(F*P*F')+trace(C1*PTr*C1')-2*trace(F*PTM*C1');
     end
     
     %% plots
     % plot PD-BT posterior covariance error bound    
     subplot(3,2,2*scale_count)
-    semilogy(r_vals,norm(L_pr)^2*sqrt(Trace_vals),'^--','Color','#DC267F','LineWidth',2); hold on
-    semilogy(r_vals,norm(L_pr)^2*sqrt(Trace_vals_inf),'+--','Color','#648FFF','LineWidth',2);
-    semilogy(r_vals,F_Dist_abs,'v-','Color','#DC267F','LineWidth',2)
-    semilogy(r_vals,F_Dist_inf,'x-','Color','#648FFF','LineWidth',2)
+    semilogy(r_vals,norm(Gamma_pr_root)^2*sqrt(Trace_vals_TL),'--','Color','#DC267F','LineWidth',3); hold on
+    semilogy(r_vals,norm(Gamma_pr_root)^2*sqrt(Trace_vals_inf),'--','Color','#648FFF','LineWidth',2);
+    semilogy(r_vals,F_Dist_TL,'-','Color','#DC267F','LineWidth',3)
+    semilogy(r_vals,F_Dist_inf,'-','Color','#648FFF','LineWidth',2)
     xlim([0 rmax])
     ylim([1e-6 1e+7])
     set(gca,'fontsize',13,'ticklabelinterpreter','latex')
@@ -197,16 +198,16 @@ for scale_count = 1:length(scaling_factors)
     end
     if scale_count==length(scaling_factors)
         xlabel('$r$','interpreter','latex','fontsize',13)
-        legend({'error bound TLBT','error bound infinite BT','$\|\mathbf{\Gamma}_\mathrm{pos}-\hat{\mathbf{\Gamma}}_\mathrm{pos}\|_F$ PD-TLBT','$\|\mathbf{\Gamma}_\mathrm{pos}-\hat{\mathbf{\Gamma}}_\mathrm{pos}\|_F$ inf PD-BT'},'interpreter','latex','fontsize',13,'Location','best')
+        legend({'error bound TLBT','error bound PD-BT','$\|\mathbf{\Gamma}_\mathrm{pos}-\hat{\mathbf{\Gamma}}_\mathrm{pos}\|_F$ PD-TLBT','$\|\mathbf{\Gamma}_\mathrm{pos}-\hat{\mathbf{\Gamma}}_\mathrm{pos}\|_F$ PD-BT'},'interpreter','latex','fontsize',13,'Location','best')
         legend boxoff
     end
 
     % plot PD-BT posterior mean error bound    
     subplot(3,2,2*scale_count-1)
-    semilogy(r_vals,norm(L_pr)^2*sqrt(Trace_vals),'^--','Color','#DC267F','LineWidth',2); hold on
-    semilogy(r_vals,norm(L_pr)^2*sqrt(Trace_vals_inf),'+--','Color','#648FFF','LineWidth',2);
-    semilogy(r_vals,mean_Dist,'v-','Color','#DC267F','LineWidth',2);
-    semilogy(r_vals,mean_Dist_inf,'x-','Color','#648FFF','LineWidth',2);
+    semilogy(r_vals,norm(Gamma_pr_root)^2*sqrt(Trace_vals_TL),'--','Color','#DC267F','LineWidth',3); hold on
+    semilogy(r_vals,norm(Gamma_pr_root)^2*sqrt(Trace_vals_inf),'--','Color','#648FFF','LineWidth',2);
+    semilogy(r_vals,mean_Dist_TL,'-','Color','#DC267F','LineWidth',3);
+    semilogy(r_vals,mean_Dist_inf,'-','Color','#648FFF','LineWidth',2);
     xlim([0 rmax])
     ylim([1e-6 1e+7])
     set(gca,'fontsize',13,'ticklabelinterpreter','latex')
@@ -216,7 +217,7 @@ for scale_count = 1:length(scaling_factors)
     end
     if scale_count==length(scaling_factors)
         xlabel('$r$','interpreter','latex','fontsize',13)
-        legend({'error bound TLBT','error bound infinite BT','$\|\mathbf{\mu}_\mathrm{pos}-\hat{\mathbf{\mu}}_\mathrm{pos}\|_2$ PD-TLBT','$\|\mathbf{\mu}_\mathrm{pos}-\hat{\mathbf{\mu}}_\mathrm{pos}\|_2$ inf PD-BT'},'interpreter','latex','fontsize',13,'Location','best')
+        legend({'error bound TLBT','error bound PD-BT','$\|\mathbf{\mu}_\mathrm{pos}-\hat{\mathbf{\mu}}_\mathrm{pos}\|_2$ PD-TLBT','$\|\mathbf{\mu}_\mathrm{pos}-\hat{\mathbf{\mu}}_\mathrm{pos}\|_2$ PD-BT'},'interpreter','latex','fontsize',13,'Location','best')
         legend boxoff
     end
 end
